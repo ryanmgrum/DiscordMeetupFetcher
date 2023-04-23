a88da9d5ea60461385c41b8d01bc17ad99d3c326
ryanmgrum <69282553+ryanmgrum@users.noreply.github.com>
2023-04-23 12:59:50 -0500

    Removed rss-parser and cheerio dependencies. Now using bent for JSON parsing. Now using moment and moment-timezone to format Date object output. Greatly simplified code, is much easier to maintain. Now delete messages based on message date, not event date in message. Modified event output to use bold and italic formatting and new data. Bot now gracefully shuts down after 10 seconds of runtime.

--- DiscordBot.js
+++ DiscordBot.js
@@ -2,13 +2,12 @@
 const Discord = require("discord.js"); // Used for interfacing with Discord
 const client = new Discord.Client(); // Used for interacting with Discord
 const config = require("./config.json"); // config file containing necessary information to function
-const Parser = require('rss-parser'); // Used for parsing RSS feeds
-const cheerio = require('cheerio'); // Used for parsing HTML-like code
+const bent = require('bent'); // Used for fetching RSS-formatted feeds
+const moment = require('moment-timezone'); // Used to easily format Date objects.
 
 module.exports = class DiscordBot {
 
     constructor() {
-        this.discordEventList = [];
         this.numOfEvents = 100;
     }
 
@@ -20,93 +19,55 @@ module.exports = class DiscordBot {
      */
      deleteOldEvents(err) {
         // First find the events to delete.
-        let deleteEvents = [];
         let channel = client.channels.cache.get(config.CHANNEL_ID);
         channel.messages.fetch({limit: this.numOfEvents}).then(messages => {
             // Iterate through the messages here with the variable "messages".
-            let regPattern = /(?:Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday), (January|February|March|April|May|June|July|August|September|October|November|December) (\d+) at (\d+):(0\d|\d+) ([AP]M)/;
             let now = new Date();
+            //now.setDate(now.getDate() + 3);
             messages.forEach(message => {
-                if (message.member.user.id == '845419198917902376' /*Discord Bot's ID*/) {
-                    // Fetch date from message contents.
-                    let datePieces = message.content.match(regPattern);
-                    let date = new Date(`${datePieces[1]} ${datePieces[2]}, ${new Date().getFullYear()} ${datePieces[5] == 'PM' ? Number(datePieces[3]) + 12 : datePieces[3]}:${datePieces[4]}`);
-                    if (date - now  < 0)
-                        //console.log('Deleting ' + message.content);
+                if (message.member.user.id == '845419198917902376' /*Discord Bot's ID*/)
+                    if (new Date(message.createdTimestamp) - now < 0)
                         message.delete(); // Delete old event message.
-                }
             });
         });
     }
 
     /** fetchMeetupEvents reads the RSS feed for the given Meetup group URL in config.json
      *  and adds it to an array that will be passed to addDiscordEvents. The array will only
-     *  contain events that are not yet in the specified guildID's channelName.
+     *  contain events that are not yet in the specified guildID's channelName (assuming this only runs once a day).
      * @param err Whether an Error occurred before calling this function.
      */
-    fetchMeetupEvents(err) {
+    async fetchMeetupEvents(err) {
         if (err !== null) {
             console.log(err);
             return;
         }
 
-        new Parser().parseURL(config.MEETUP_RSS_FEED).then(feed => {
-            let meetupEvents = [];
+        let today = new Date()
+        let meetupEvents = []
 
-            feed.items.forEach(item => {
-                // Parse item's content to get the event's start date and time.
-                let $ = cheerio.load(item.content);
-                let regPattern = /(?:Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday), (January|February|March|April|May|June|July|August|September|October|November|December) (\d+) at (\d+):(0\d|\d+) ([AP]M)/;
-                //console.log(item.content);
-                $('p').each(function(i, el) {
-                    if (el.firstChild !== null)
-                        if (el.firstChild.data != undefined)
-                            if (el.firstChild.data.match(regPattern) !== null) {
-                                let datePieces = el.firstChild.data.match(regPattern);
-                                let date = new Date(`${datePieces[1]} ${datePieces[2]}, ${new Date().getFullYear()} ${datePieces[5] == 'PM' ? (Number(datePieces[3]) === 12 ? 12 : Number(datePieces[3]) + 12) : datePieces[3]}:${datePieces[4]}`);
-                                if (date.toDateString() == new Date().toDateString())
-                                    meetupEvents.push(item);
-                            }
-                });
-            });
+        let bentJSON = bent('json');
+        let json = await bentJSON(config.MEETUP_RSS_FEED);
 
-            if (meetupEvents.length != 0) // Output new events.
-                try {
-                    this.addDiscordEvents(err, meetupEvents);
-                } catch (error) {
-                    console.log(err);
-                    this.end();
-                }
-            else { // Print message notifying no new events with current date and time.
-                let date_ob = new Date();
-    
-                // current date
-                // adjust 0 before single digit date
-                let day = ("0" + date_ob.getDate()).slice(-2);
-    
-                // current month
-                let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-    
-                // current year
-                let year = date_ob.getFullYear();
-    
-                // current hours
-                let hours = date_ob.getHours();
-    
-                // current minutes
-                let minutes = date_ob.getMinutes();
-    
-                // current seconds
-                let seconds = date_ob.getSeconds();
-                console.log(year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds + ' Nothing new at this time.');
-                this.end();
+        json.responses[0].value.forEach(event => {
+            let eventTime = new Date(event.time);
+            //console.log("Event Name " + event.name + " at " + eventTime + ".");
+            if (eventTime.toDateString() == today.toDateString()) {
+                //console.log("Posting event to Discord!");
+                meetupEvents.push(event);
             }
         });
+
+        if (meetupEvents.length == 0)
+            console.log(today + ": Nothing new at this time.");
+        else
+            this.addDiscordEvents(err, meetupEvents);
+        
+        this.end();
     }
 
     /** addDiscordEvents writes messages from the passed-in eventList array of new events read
      *  from the given Meetup group RSS feed in config.json.
-     *  Precondition: The events do not exist in discordEventList.
      * @param err Whether an Error occurred before processing this function.
      * @param eventList An Array containing a list of new events read from the Meetup RSS feed
      * (read from config.json).
@@ -117,56 +78,76 @@ module.exports = class DiscordBot {
             return;
         }
 
-        let count = 0;
-        eventList.forEach(e => {
-            let processList = async (e) => {
-                let output = e.title.trim() + '\n\n';
+        eventList.forEach(event => {
+            /** Output the event in the following format:
+             * Event Name
+             *
+             * When: EVENT_START_DATETIME
+             * Where: NAME, ADDRESS_1, CITY, STATE, ZIP, LOCALIZED_COUNTRY_NAME (or Online if is_online_event is true)
+             * RSVP Slots Available: RSVP_LIMIT
+             * Hosts: HOST_NAMES
+             * Description: PLAIN_TEXT_NO_IMAGES_DESCRIPTION
+             * Event Link: LINK
+             */
+            let output = "**" + event.name + "**\n\n";
+            output += "*When*: " + moment(new Date(event.time)).tz("America/Chicago").format("ddd MMM Do YYYY hh:mm:ss A zz") + "\n";
 
-                let $ = cheerio.load(e.content);
-                let regPattern = /(?:Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday), (January|February|March|April|May|June|July|August|September|October|November|December) (\d+) at (\d+):(0\d|\d+) ([AP]M)/;
-                $('p').each(function(i, el) {
-                    if (el.firstChild !== null && el.firstChild != undefined)
-                        el.children.forEach((child) => {
-                            if (child.data != undefined)
-                                if (
-                                    (child.type == 'text' &&
-                                     child.data != e.guid &&
-                                     child.data.match(/^\d+$/) === null &&
-                                     child.data.match('Introverts Hangout') === null
-                                    ) ||
-                                    child.data.match(regPattern) !== null
-                                )
-                                    output += child.data + '\n';
-                        });
-                });
+            if (event.is_online_event == false) {
+                let addressPieces = []
+                if (event.venue.name != "")
+                    addressPieces.push(event.venue.name);
+                if (event.venue.address_1 != "")
+                    addressPieces.push(event.venue.address_1);
+                if (event.venue.city != "")
+                    addressPieces.push(event.venue.city);
+                if (event.venue.state != "")
+                    addressPieces.push(event.venue.state);
+                if (event.venue.zip != "")
+                    addressPieces.push(event.venue.zip);
+                if (event.venue.localized_country_name != "")
+                    addressPieces.push(event.venue.localized_country_name);
 
-                output += '\n' + e.guid;
-
-                let channel = client.channels.cache.get(config.CHANNEL_ID);
-                if (channel != undefined) {
-                    //console.log('Writing event "' + e.title.trim() + '"\n');
-                    await channel.send(output);
-                }
-
-                count++;
-                if (count == eventList.length)
-                    this.end();
+                output += "*Where*: " + addressPieces.join(", ") + "\n";
+            } else {
+                output += "*Where*: Online\n";
             }
-            processList(e);
+
+            output += "*RSVP Slots Available*: " + event.rsvp_limit + "\n";
+
+            let hosts = []
+            event.event_hosts.forEach(host => {
+                hosts.push(host.name);
+            });
+            output += "*Host(s)*: " + hosts.join(", ") + "\n";
+
+            output += "*Description*: \"" + event.plain_text_no_images_description + "\"\n"
+
+            output += "*Event Link*: " + event.link + "\n\n"
+
+            //console.log(output);
+
+            let channel = client.channels.cache.get(config.CHANNEL_ID);
+            channel.send(output);
         });
     }
 
     /** run is used to start the DiscordBot.
      */
     run() {
-        // Log client in to MeetupEventFetcher Discord bot.
+        // Log client into MeetupEventFetcher Discord bot.
         client.login(config.BOT_TOKEN).then(
-            // Check for new Meetup events.
             client.once('ready', () => {
                 this.deleteOldEvents(null);
                 this.fetchMeetupEvents(null);
             })
         );
+
+        //console.time("Discord bot runtime.");
+        
+        client.setTimeout(() => {
+            //console.timeEnd("Discord bot runtime.");
+            this.end();
+        }, 10000);
     }
 
     /** end is used to stop the DiscordBot.
